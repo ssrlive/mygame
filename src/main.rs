30e99c5be991fbd35d9b720f77dba35a9f0e4c1a
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-01-14 16:39:47 +0800

    resource timer added

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(_time: Res<Time>, query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
@@ -24,10 +26,14 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         app.add_systems(Update, (update_people, greet_people).chain());
     }
