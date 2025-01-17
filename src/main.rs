31d82f46d54fcc02b07561639667ea6166cc0d1f
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-01-17 14:23:39 +0800

    customize DefaultPlugins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Space Invaders".to_string(),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        resolution: Vec2::new(768.0, 512.0).into(),
+                        ..Window::default()
+                    }),
+                    ..WindowPlugin::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .run();
 }
