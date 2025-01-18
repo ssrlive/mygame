3b5cbbd656c028468736d53b31edc20c29531b19
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-01-18 15:31:23 +0800

    main window

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
+use rand::{rngs::ThreadRng, thread_rng, Rng};
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Flappy Bird".to_string(),
+                        position: WindowPosition::Centered(MonitorSelection::Primary),
+                        resolution: Vec2::new(800.0, 600.0).into(),
+                        ..Window::default()
+                    }),
+                    ..WindowPlugin::default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .run();
 }
