a5730ee3deb3c9fd627daf5a13cd68154f8389ad
ssrlive <30760636+ssrlive@users.noreply.github.com>
2025-01-18 13:39:36 +0800

    now we can kill the aliens

--- src/alien.rs
+++ src/alien.rs
@@ -12,13 +12,21 @@ impl Plugin for AlienPlugin {
 }
 
 #[derive(Component)]
-pub struct Alien;
+pub struct Alien {
+    pub dead: bool,
+    pub original_position: Vec3,
+}
+
+/// a marker component to prevent querying any dead aliens in our updates after they have already died
+#[derive(Component)]
+pub struct Dead;
 
 #[derive(Resource)]
 pub struct AlienManager {
     pub direction: f32,
     pub shift_aliens_down: bool,
     pub dist_from_boundary: f32,
+    pub reset: bool,
 }
 
 const WIDTH: i32 = 10;
@@ -32,6 +40,7 @@ fn setup_aliens(mut commands: Commands, asset_server: Res<AssetServer>, resoluti
         direction: 1.0,
         shift_aliens_down: false,
         dist_from_boundary: 0.0,
+        reset: false,
     });
     let alien_texture = asset_server.load("alien.png");
     for x in 0..WIDTH {
@@ -43,34 +52,66 @@ fn setup_aliens(mut commands: Commands, asset_server: Res<AssetServer>, resoluti
             commands.spawn((
                 Sprite::from_image(alien_texture.clone()),
                 Transform::from_translation(position).with_scale(Vec3::splat(resolution.pixel_ratio)),
-                Alien,
+                Alien {
+                    dead: false,
+                    original_position: position,
+                },
             ));
         }
     }
 }
 
 fn update_aliens(
-    mut alien_query: Query<(&Alien, &mut Transform)>,
+    mut commands: Commands,
+    // only query aliens that are still alive
+    mut alien_query: Query<(Entity, &Alien, &mut Transform, &mut Visibility), Without<Dead>>,
     mut alien_manager: ResMut<AlienManager>,
     resolution: Res<Resolution>,
     time: Res<Time>,
 ) {
-    for (_alien, mut transform) in alien_query.iter_mut() {
+    for (entity, alien, mut transform, mut visibility) in alien_query.iter_mut() {
         transform.translation.x += time.delta_secs() * alien_manager.direction * SPEED;
         if transform.translation.x.abs() > resolution.screen_dimensions.x * 0.5 {
             alien_manager.shift_aliens_down = true;
             alien_manager.dist_from_boundary = resolution.screen_dimensions.x * alien_manager.direction * 0.5 - transform.translation.x;
         }
+        if alien.dead {
+            commands.entity(entity).insert(Dead);
+            *visibility = Visibility::Hidden;
+        } else {
+            *visibility = Visibility::Visible;
+        }
+        // if the alien has made it out of the bottom of the screen, we have lost the game and should reset
+        if transform.translation.y < -resolution.screen_dimensions.y * 0.5 {
+            alien_manager.reset = true;
+        }
     }
 }
 
-fn manage_alien_logic(mut alien_query: Query<(&mut Alien, &mut Transform)>, mut alien_manager: ResMut<AlienManager>) {
+fn manage_alien_logic(
+    mut commands: Commands,
+    mut alien_query: Query<(Entity, &mut Alien, &mut Transform)>,
+    mut alien_manager: ResMut<AlienManager>,
+) {
     if alien_manager.shift_aliens_down {
+        // reverse direction and move aliens downwards
         alien_manager.shift_aliens_down = false;
         alien_manager.direction *= -1.0;
-        for (_alien, mut transform) in alien_query.iter_mut() {
+        for (_entity, _alien, mut transform) in alien_query.iter_mut() {
             transform.translation.x += alien_manager.dist_from_boundary;
             transform.translation.y -= ALIEN_SHIFT_AMOUNT;
+        }
+    }
+    if alien_manager.reset {
+        alien_manager.reset = false;
+        alien_manager.direction = 1.0;
+        for (entity, mut alien, mut transform) in alien_query.iter_mut() {
+            transform.translation = alien.original_position;
+            if alien.dead {
+                // revive the alien from dead unit pool
+                alien.dead = false;
+                commands.entity(entity).remove::<Dead>();
+            }
         }
     }
 }
--- src/projectile.rs
+++ src/projectile.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 
-use crate::resolution::Resolution;
+use crate::{
+    alien::{Alien, Dead},
+    resolution::Resolution,
+};
 
 pub struct ProjectilePlugin;
 
@@ -29,4 +32,22 @@ fn update_projectiles(
     }
 }
 
-fn update_alien_interactions() {}
+const BULLET_RADIUS: f32 = 24.0;
+
+fn update_alien_interactions(
+    mut alien_query: Query<(&mut Alien, &Transform), Without<Dead>>,
+    mut projectile_query: Query<(Entity, &Transform), With<Projectile>>,
+    mut commands: Commands,
+) {
+    for (mut alien, alien_transform) in alien_query.iter_mut() {
+        for (projectile_entity, projectile_transform) in projectile_query.iter_mut() {
+            let projectile_position = Vec2::new(projectile_transform.translation.x, projectile_transform.translation.y);
+            let alien_position = Vec2::new(alien_transform.translation.x, alien_transform.translation.y);
+            if Vec2::distance(projectile_position, alien_position) < BULLET_RADIUS {
+                alien.dead = true;
+                // best to not despawn in the query but the warning doesn't break the game so I don't mind too mach
+                commands.entity(projectile_entity).despawn();
+            }
+        }
+    }
+}
